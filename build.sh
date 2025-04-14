32e6b3d3f60dadafcbd94307c0312be06416170d
Michael Adams <mtadams42@gmail.com>
2025-04-13 19:10:30 -0700

    Move to docs for github pages

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-minify index.html -o ./dist/index.html
-minify script.js -o ./dist/script.js
-minify style.css -o ./dist/style.css
+minify index.html -o ./docs/index.html
+minify script.js -o ./docs/script.js
+minify style.css -o ./docs/style.css
